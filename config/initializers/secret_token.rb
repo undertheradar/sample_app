c69edc0b32bd261823ce1d2dddad3aff8e221095
XP Pro <winux@unm.edu>
2011-10-28 10:19:04 +0400

    Initial sample app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '80791a6e6c7349fd11f4c86f710c17a1cdd6d6322f373d8c782aa7cc6fc82b209cb132b79d4798700acf540df2f26a3fb9fd70f729d7cf575ee157568bd609ab'
